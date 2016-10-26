5a055dd182b83056657b5a59cedce70f36a423b3
Bui, Giao Long <giaolong.bui@student.fhws.de>
2016-10-26 15:01:54 +0200

    Aufgabe 1

---
+++ src/main/java/Blatt1/Test.java
@@ -0,0 +1,25 @@
+package Blatt1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by Long Bui on 26.10.16.
+ * E-Mail: giaolong.bui@student.fhws.de
+ */
+public class Test
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            RGBPicture testPicture = new RGBPicture(new FileInputStream(new File("test2.ppm")));
+            System.out.println(testPicture.toString());
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
